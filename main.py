3265f5326be038630481ee7286382eac250054d6
Justin Monge <justin.mnge@gmail.com>
2024-09-02 10:37:05 -0300

    Added circleshape.py & player.py. Modified constants.py & main.py

--- main.py
+++ main.py
@@ -1,22 +1,23 @@
 import pygame
 from constants import *
+from player import *
 
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
     print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
     
     while True:
         for event in pygame.event.get(): # handle events
             if event.type == pygame.QUIT: # update game state
                 return
             
-        pygame.Surface.fill(screen, (0, 0, 0)) # draw everythong
+        screen.fill("black") # draw everything
+        player.draw(screen)
         pygame.display.flip() # update display
         dt = clock.tick(60) / 1000 # control frame rate and get dt
     
