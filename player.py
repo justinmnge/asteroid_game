c4edbfb70daa35a57dc3cd3046f5a6b3e728ee56
Justin Monge <justin.mnge@gmail.com>
2024-09-02 11:20:46 -0300

    added new method to Player class called rotate. It will add to player's current rotation

--- player.py
+++ player.py
@@ -1,6 +1,7 @@
 import pygame
 from circleshape import *
 from constants import *
+from main import *
 
 class Player(CircleShape):
     def __init__(self, x, y):
@@ -18,3 +19,14 @@ class Player(CircleShape):
         b = self.position - forward * self.radius - right
         c = self.position - forward * self.radius + right
         return [a, b, c]
+    
+    def rotate(self, dt):
+        self.rotation += (PLAYER_TURN_SPEED * dt)
+    
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            # ?
+        if keys[pygame.K_d]:
+            # ?
