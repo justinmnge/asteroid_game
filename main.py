89586ea68b84fadd9edfa01ce98dd4dc9c8a166a
Justin Monge <justin.mnge@gmail.com>
2024-09-01 21:21:09 -0300

    updated main.py to draw a max of 60 fps

--- main.py
+++ main.py
@@ -5,17 +5,20 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     
     while True:
-        pygame.Surface.fill(screen, (0, 0, 0))
-        pygame.display.flip()
-        
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+        for event in pygame.event.get(): # handle events
+            if event.type == pygame.QUIT: # update game state
                 return
+            
+        pygame.Surface.fill(screen, (0, 0, 0)) # draw everythong
+        pygame.display.flip() # update display
+        dt = clock.tick(60) / 1000 # control frame rate and get dt
     
 if __name__ == "__main__":
     main()
