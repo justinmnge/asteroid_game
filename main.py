3ab68fd30e6db9c431aad9bc48676164e6803931
Justin Monge <justin.mnge@gmail.com>
2024-09-02 21:31:20 -0300

    Added asteroid.py and asteroidfield.py. We now have asteroids randomly spawning in various sizes!

--- main.py
+++ main.py
@@ -1,13 +1,26 @@
 import pygame
 from constants import *
 from player import *
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+    
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroidfield = AsteroidField()
+
     dt = 0
     print("Starting asteroids!")
     
@@ -15,12 +28,18 @@ def main():
         for event in pygame.event.get(): # handle events
             if event.type == pygame.QUIT: # update game state
                 return
-            
+        
+        for obj in updatable:    
+            obj.update(dt)
+        
         screen.fill("black") # draw everything
-        player.draw(screen)
+        
+        for obj in drawable:
+            obj.draw(screen)
+        
         pygame.display.flip() # update display
+        
         dt = clock.tick(60) / 1000 # control frame rate and get dt
-        player.update(dt)
     
 if __name__ == "__main__":
     main()
